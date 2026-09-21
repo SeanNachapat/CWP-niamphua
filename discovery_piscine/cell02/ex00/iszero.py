a144947bac895ae81e63bd8c217d0b50dc32f808
Seannachapat <seannachapat@gmail.com>
2026-09-21 17:10:23 +0700

    cell02-ex00

---
+++ discovery_piscine/cell02/ex00/iszero.py
@@ -0,0 +1,9 @@
+try:
+	num = float(input())	
+except:
+	print("This is not a number!")
+else:
+	if (num == 0):
+		print("This number is equal to zero.") 
+	else:
+		print("This number is different from zero.")
